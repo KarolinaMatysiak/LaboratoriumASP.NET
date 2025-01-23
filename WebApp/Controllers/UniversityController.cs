6d5ea41001e2e669e8b2bfb50cc32f8e29475e52
karolincia <karolina.matysiak@microsoft.wsei.edu.pl>
2025-01-23 20:05:43 +0100

    FinalProject\6:Add authentication and autorization for University controller (admin,user)

--- WebApp/Controllers/UniversityController.cs
+++ WebApp/Controllers/UniversityController.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +7,7 @@ using WebApp.Models.University;
 
 namespace WebApp.Controllers
 {
+    [Authorize(Roles = "admin,user")]
     public class UniversityController : Controller
     {
         private readonly UniversityDbContext _context;
@@ -21,6 +19,7 @@ namespace WebApp.Controllers
             _universityService = universityService;
         }
         
+        [AllowAnonymous]
         public async Task<IActionResult> Index([FromQuery] PaginationModel pagination)
         {
             var recordsCount = await _context
@@ -105,127 +104,7 @@ namespace WebApp.Controllers
             });
         }
 
-        // GET: University/Create
-        public IActionResult Create()
-        {
-            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id");
-            return View();
-        }
-
-        // POST: University/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,CountryId,UniversityName")] UniversityEntity universityEntity)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(universityEntity);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-
-            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", universityEntity.CountryId);
-            return View(universityEntity);
-        }
-
-        // GET: University/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var university = await _context.Universities.FindAsync(id);
-            if (university == null)
-            {
-                return NotFound();
-            }
-
-            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", university.CountryId);
-            return View(university);
-        }
-
-        // POST: University/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id,
-            [Bind("Id,CountryId,UniversityName")] UniversityEntity universityEntity)
-        {
-            if (id != universityEntity.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(universityEntity);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UniversityExists(universityEntity.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
-                return RedirectToAction(nameof(Index));
-            }
-
-            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Id", universityEntity.CountryId);
-            return View(universityEntity);
-        }
-
-        // GET: University/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var university = await _context.Universities
-                .Include(u => u.Country)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (university == null)
-            {
-                return NotFound();
-            }
-
-            return View(university);
-        }
-
-        // POST: University/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var university = await _context.Universities.FindAsync(id);
-            if (university != null)
-            {
-                _context.Universities.Remove(university);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UniversityExists(int id)
-        {
-            return _context.Universities.Any(e => e.Id == id);
-        }
-
+        [Authorize(Roles = "admin")]
         public IActionResult AddRanking(int universityId)
         {
             var dbRankingSystems = _context.RankingSystems.ToList();
@@ -247,6 +126,7 @@ namespace WebApp.Controllers
             return View(model);
         }
 
+        [Authorize(Roles = "admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddRanking(AddRankingModel model)
